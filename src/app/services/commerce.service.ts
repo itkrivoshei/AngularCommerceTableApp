5112fb1c4b36b0c96a2b834baca85b5c394b605b
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-21 19:44:18 +0100

    Add HttpClient and getRandomCommerces method to CommerceService

--- src/app/services/commerce.service.ts
+++ src/app/services/commerce.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommerceService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getRandomCommerces(): Observable<any> {
+    return this.http.get(
+      'https://random-data-api.com/api/commerce/random_commerce?size=100'
+    );
+  }
 }
