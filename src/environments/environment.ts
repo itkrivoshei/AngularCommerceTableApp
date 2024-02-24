d50923b2ce08eb87634922de180414a74c11ad29
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-24 21:34:17 +0100

    Update API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl:
-    'https://cors-anywhere.herokuapp.com/https://random-data-api.com/api/commerce/random_commerce?size=100',
+    'https://hdm2f1y062.execute-api.eu-north-1.amazonaws.com/default/CommerceAPIProxy',
 };
