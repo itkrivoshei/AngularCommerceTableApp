e0927d037b5a2d0281e4114f0242c47f9afa56e4
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-24 17:58:17 +0100

    Add MatButtonModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CommerceTableComponent } from './components/commerce-table/commerce-table.component';
 import * as fromCommerce from './store/reducers/commerce.reducer';
 import { CommerceEffects } from './store/effects/commerce.effects';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, CommerceTableComponent],
@@ -23,8 +23,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     EffectsModule.forRoot([CommerceEffects]),
     MatTableModule,
     BrowserAnimationsModule,
+    MatButtonModule,
   ],
-  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
